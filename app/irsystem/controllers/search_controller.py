f201044ba37f683c51aa24109a6180548cf4ddde
Destiny Malloy <dam359@cornell.edu>
2021-05-04 16:55:31 -0400

    created page to explain scores

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -29,7 +29,8 @@ print("memory beg", mac_memory_in_MB)
 def search():
     search_bar = request.args.get('search')
     query = search_bar
-    transmission = request.args.get('transmission') # if manual or automatic add it to query
+    # if manual or automatic add it to query
+    transmission = request.args.get('transmission')
     two_doors = request.args.get("2dr")
     four_doors = request.args.get("4dr")
     conv = request.args.get("conv")
@@ -127,39 +128,42 @@ print("memory after load", mac_memory_in_MB)
 
 stemmer = SnowballStemmer("english")
 
+
 def get_image(car_name):
     car_toks = car_name.split(" ")
     url = 'https://www.google.com/search?q='
     for i, tok in enumerate(car_toks):
         if (i == len(car_toks)-1):
-            url+=tok+"&tbm=isch"
+            url += tok+"&tbm=isch"
         else:
-            url+=tok+'+'
+            url += tok+'+'
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'html.parser')
 
-    car_image = soup.find('img', attrs={'class' : 't0fcAb'}).get('src')
+    car_image = soup.find('img', attrs={'class': 't0fcAb'}).get('src')
 
     return car_image
+
 
 def get_link(car_name):
     car_toks = car_name.split(" ")
     url = 'https://www.google.com/search?q='
     for i, tok in enumerate(car_toks):
         if (i == len(car_toks)-1):
-            url+=tok
+            url += tok
         else:
-            url+=tok+'+'
+            url += tok+'+'
     url += "&num=" + str(5)
     html_text = requests.get(url).text
     soup = BeautifulSoup(html_text, 'html.parser')
-    
-    #Below code snippet from: https://predictivehacks.com/how-to-scrape-google-results-for-free-using-python/
-    result = soup.find_all('div', attrs = {'class': 'ZINbbc'})
-    results = [re.search(r'\/url\?q\=(.*)\&sa',str(i.find('a', href = True)['href'])) for i in result if "url" in str(i)]
-    links=[i.group(1) for i in results if i != None]
-    #end of code snippet
-    
+
+    # Below code snippet from: https://predictivehacks.com/how-to-scrape-google-results-for-free-using-python/
+    result = soup.find_all('div', attrs={'class': 'ZINbbc'})
+    results = [re.search(r'\/url\?q\=(.*)\&sa', str(i.find('a', href=True)['href']))
+               for i in result if "url" in str(i)]
+    links = [i.group(1) for i in results if i != None]
+    # end of code snippet
+
     link = links[0]
     return link
 
@@ -175,14 +179,15 @@ def get_ranked(query):
         # print("in else")
         ranked_lst = [(id_to_car[i], round(ratings[i], 2), round(sim_mat[i], 2), get_image(id_to_car[i]), get_link(id_to_car[i]))
                       for i in np.argsort(sim_mat)[::-1][:5]]
-        ranked_lst = sorted(ranked_lst, key = lambda x: (x[2], x[1]), reverse=True)
+        ranked_lst = sorted(
+            ranked_lst, key=lambda x: (x[2], x[1]), reverse=True)
 
         # print("list is ", ranked_lst)
 
         return ranked_lst
     # printing out to see the cosine scores
-    #for i in np.argsort((sim_mat))[::-1][:5]:
-        #print(sim_mat[i])
+    # for i in np.argsort((sim_mat))[::-1][:5]:
+        # print(sim_mat[i])
 
 
 # data is the car name
@@ -203,29 +208,33 @@ def cosine_sim(query):
 
     q_vec_reviews = tfidf_vec_reviews.transform(stemmed_query)
     q_vec_titles = tfidf_vec_titles.transform(stemmed_query)
-    q_hat_rev = q_vec_reviews@U_rev[:,:k_rev]
-    q_hat_tit = q_vec_titles@U_tit[:,:k_tit]
+    q_hat_rev = q_vec_reviews@U_rev[:, :k_rev]
+    q_hat_tit = q_vec_titles@U_tit[:, :k_tit]
 
-    first_mul = np.matmul(np.diag(S_rev[:k_rev]), V_T_rev[:k_rev]) # 100 by 15609
+    first_mul = np.matmul(
+        np.diag(S_rev[:k_rev]), V_T_rev[:k_rev])  # 100 by 15609
 
     num_rev = np.matmul(np.transpose(first_mul), np.transpose(q_hat_rev))
-    #print("second_mul", num_rev.shape) # 15609 by 1
+    # print("second_mul", num_rev.shape) # 15609 by 1
 
     norms = np.apply_along_axis(np.linalg.norm, 0, first_mul)
     #print("norms", norms.shape)
 
-    norm_q = np.linalg.norm(q_hat_rev) # this is a float
+    norm_q = np.linalg.norm(q_hat_rev)  # this is a float
 
-    denom_rev = norm_q * norms # 15609
+    denom_rev = norm_q * norms  # 15609
     denom_rev = denom_rev.reshape((denom_rev.shape[0], 1))
     #print("denom rev", denom_rev.shape)
 
-    rev_sc = np.divide(num_rev, denom_rev, out=np.zeros_like(num_rev), where=denom_rev!=0)
+    rev_sc = np.divide(num_rev, denom_rev, out=np.zeros_like(
+        num_rev), where=denom_rev != 0)
     #print("rev sc", rev_sc.shape)
 
-    first_num_tit = np.matmul(np.diag(S_tit[:k_tit]),V_T_tit[:k_tit]) # 325 by 15609
+    first_num_tit = np.matmul(
+        np.diag(S_tit[:k_tit]), V_T_tit[:k_tit])  # 325 by 15609
 
-    num_tit = np.matmul(np.transpose(first_num_tit), np.transpose(q_hat_tit)) # 15609 by 1
+    num_tit = np.matmul(np.transpose(first_num_tit),
+                        np.transpose(q_hat_tit))  # 15609 by 1
 
     norms_tit = np.apply_along_axis(np.linalg.norm, 0, first_num_tit)
 
@@ -234,9 +243,20 @@ def cosine_sim(query):
     denom_tit = norm_q_tit * norms_tit
     denom_tit = denom_tit.reshape((denom_tit.shape[0], 1))
 
-    tit_sc = np.divide(num_tit, denom_tit, out=np.zeros_like(num_tit), where=denom_tit!=0)
+    tit_sc = np.divide(num_tit, denom_tit, out=np.zeros_like(
+        num_tit), where=denom_tit != 0)
 
     #print("rev_sc", rev_sc[:5])
     #print("tit_sc", tit_sc[:5])
     sim = (0.3 * rev_sc) + (0.7 * tit_sc)
     return sim
+
+
+@irsystem.route('/explanation')
+def explanation():
+    return render_template('explanation.html', name=project_name, netid=net_id)
+
+
+# def back():
+#     back_button = request.args.get('search')
+#     return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
